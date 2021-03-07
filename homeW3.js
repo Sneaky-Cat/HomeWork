73e37692310b673c5a108d7000bf5b17eb98999e
Sneaky-Cat <78099932+Sneaky-Cat@users.noreply.github.com>
2021-03-07 16:40:17 +0200

    Update homeW3.js

--- homeW3.js
+++ homeW3.js
@@ -64,3 +64,41 @@ let MatrixTranspose = function(array, totallength){
       console.log(number);
     }
     MatrixStolbec([[8,3,8],[7,9,9],[3,2,7]], 3);
+
+//3.6 Удалить из массива все столбцы которые не имеют ни одного нулевого элемента и сумма которых положительна - оформить в виде функции
+
+  let MatrixDeleteStolbec = function(array, totallength){
+    let counter = 0;
+    for (let b = 0; b<totallength;){
+      let sum = 0;
+      let zeroFactor = false;
+      let deleteFactor = false;
+        for (let j = 0; j<totallength; j++){
+          sum += array[j][b];
+          console.log(sum);
+          if (array[j][b] === 0){
+              zeroFactor = true;
+          }
+          if ( j === totallength-1 ){
+              if ((sum > 0 && zeroFactor === false)){
+                  deleteFactor = true;
+              }
+          }
+        }
+        if (deleteFactor === true){
+            for (let c = 0; c<totallength; c++){
+                array[c].splice(b, 1); 
+            }
+            counter+=1;
+        }
+        else if(deleteFactor === false){
+            b++;
+            counter+=1;
+        }
+        if (counter === totallength){
+            break;
+        }
+    }
+    console.log(array);
+};
+MatrixDeleteStolbec([[-5,3,8],[7,9,5],[3,2,0]], 3);
